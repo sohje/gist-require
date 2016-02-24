5f39ee5fed3b39f568ca77aef1f7eef845cdf381
Nicholas <nikolay.spiridonov@gmail.com>
2016-02-24 18:34:39 +0300

    travis, broken tls?

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ test.cb(t => {
     let url = 'https://gist.github.com/sohje/73a80333911026a1be25'
     t.plan(3);
     gistr(url, (err, data) => {
+        console.log(data, err)
         t.ok(data);
         t.notThrows(data);
         t.ifError(err);
