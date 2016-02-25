0e5565241a28681b147e61415c1428463a333545
Nicholas <nikolay.spiridonov@gmail.com>
2016-02-25 14:18:57 +0300

    Test logic fix

--- test.js
+++ test.js
@@ -7,12 +7,13 @@ test(t => {
 
 test.cb(t => {
     let url = 'https://gist.github.com/sohje/73a80333911026a1be25'
-    t.plan(1);
     gistr(url, (err, data) => {
-        // t.ok(data);
-        // t.notThrows(data);
+        if (data)  {
+            t.ok(data);
+            t.notThrows(data);
+        }
         // Github API rate limiting for travis servers...
-        t.ifError(!err);
+        if (err) t.ifError(err);
         t.end();
     })
 })
